9c66de417168e2622a705e17be7ad4fbf7f1b9b1
suvobd100 <makbarbd@gmail.com>
2024-12-06 23:15:39 +0600

    Quota Movement card js complite with validation and model

--- assets/js/index.js
+++ assets/js/index.js
@@ -93,7 +93,27 @@ document.getElementById('btnFeniDonation').addEventListener('click',function () 
             return;
 
     }
-  
+     
+
+    // deduct from cash in hand
+    const cashInHandAmt=Number(document.getElementById('TxtCashInHand').innerText);
+
+    if (cashInHandAmt < feniDonationAmt){
+        alert('Insufficient cash in hand for transaction');
+        document.getElementById('valFeniAmt').focus();
+
+        return;
+
+    }
+
+
+    // alert(cashInHandAmt)
+    // deduct from cash in hand
+
+    let cashInhandDeductAmt= cashInHandAmt-feniDonationAmt;
+
+    document.getElementById('TxtCashInHand').innerText= cashInhandDeductAmt;
+
     
     // set text value
 
@@ -105,22 +125,11 @@ document.getElementById('btnFeniDonation').addEventListener('click',function () 
    
     // clear input value
     document.getElementById('valFeniAmt').value='';
-    
 
-
-    // deduct from cash in hand
-    const cashInHandAmt=Number(document.getElementById('TxtCashInHand').innerText);
-
-    // alert(cashInHandAmt)
-    // deduct from cash in hand
-
-    let cashInhandDeductAmt= cashInHandAmt-feniDonationAmt;
-
-    document.getElementById('TxtCashInHand').innerText= cashInhandDeductAmt;
 
   // Show the modal and set the donation amount
   document.getElementById('txtDonationModal').innerText = feniDonationAmt;
-  
+
   // add modal
   document.getElementById('myModal').classList.add('modal-open');  
     
@@ -134,4 +143,89 @@ document.getElementById('btnFeniDonation').addEventListener('click',function () 
 document.getElementById('closeModalBtn').addEventListener('click', function () {
     document.getElementById('myModal').classList.remove('modal-open');
     document.getElementById('valFeniAmt').focus();
+});
+
+// ---------------Quota Movement Card start----------------
+
+document.getElementById('btnQmovmentDonation').addEventListener('click',function () {
+    // getting previous amount
+    let qMovementDonationAmtPrv=Number(document.getElementById('txtQmovementAmt').innerText);
+
+        alert(qMovementDonationAmtPrv);
+
+    // getting value from input by function
+    
+        const qMovementDonationAmt=getInputValueById('valQmovementAmt');
+
+    // chacking negative or other then number
+
+    if (qMovementDonationAmt<0 || isNaN(qMovementDonationAmt)){
+
+            alert('Invalid Input');
+            
+            document.getElementById('valQmovementAmt').value='';
+            document.getElementById('valQmovementAmt').focus();
+
+            return;
+
+    } else if(qMovementDonationAmt<20 ){
+
+            alert('Minimum Donation Amount 20 BDT');
+            
+            document.getElementById('valQmovementAmt').value='';
+            document.getElementById('valQmovementAmt').focus();
+
+            return;
+
+    }
+    
+    
+    // deduct from cash in hand
+    
+    const cashInHandAmt = Number(document.getElementById('TxtCashInHand').innerText);
+
+    //  check sufficient cash in hand
+
+    if (cashInHandAmt < qMovementDonationAmt){
+        alert('Insufficient cash in hand for transaction');
+
+        return;
+
+    }
+
+    // alert(cashInHandAmt)
+    // deduct from cash in hand
+
+    let cashInhandDeductAmt= (cashInHandAmt - qMovementDonationAmt);
+
+    document.getElementById('TxtCashInHand').innerText= cashInhandDeductAmt;
+
+    // set text value
+
+    let newTotalAmount= (qMovementDonationAmt + qMovementDonationAmtPrv);
+    alert(newTotalAmount)
+
+    document.getElementById('txtQmovementAmt').innerText= newTotalAmount;
+   
+   
+    // clear input value
+    document.getElementById('valQmovementAmt').value='';
+
+
+  // Show the modal and set the donation amount
+  document.getElementById('txtDonationModal').innerText = qMovementDonationAmt;
+
+  // add modal
+  document.getElementById('myModal').classList.add('modal-open');  
+    
+
+})
+
+// end Qmovment
+
+
+// Close modal functionality
+document.getElementById('closeModalBtn').addEventListener('click', function () {
+    document.getElementById('myModal').classList.remove('modal-open');
+    document.getElementById('valQmovementAmt').focus();
 });
